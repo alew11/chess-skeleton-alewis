91850c665a19c445f4ed9a58457697a6fe5f3064
alew11 <ajlewis1113@gmail.com>
2014-10-16 12:20:16 -0500

    Tests for king safety

--- src/main/java/chess/pieces/Queen.java
+++ src/main/java/chess/pieces/Queen.java
@@ -77,4 +77,5 @@ public class Queen extends Piece{
 		
 		return possiblePositions;
 	}
+	
 }
---
+++ src/test/java/chess/KingSafetyTest.java
@@ -0,0 +1,71 @@
+package chess;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import chess.pieces.Bishop;
+import chess.pieces.King;
+import chess.pieces.Pawn;
+
+public class KingSafetyTest {
+
+	private GameState state;
+
+	@Before
+	public void setUp() throws Exception {
+		state = new GameState();
+
+	}
+
+	@Test
+	public void testPins() {
+		
+		Map<Position, Set<Position>> actualPossible =  new HashMap<Position, Set<Position>>();
+    	Set<Position> positionsTo = new HashSet<Position>();
+		
+		state.placePiece(new King(Player.White), new Position("d1"));
+    	state.placePiece(new Pawn(Player.White), new Position("e2"));
+    	state.placePiece(new Bishop(Player.Black), new Position("g4"));
+    	
+    	positionsTo.add(new Position("c1"));
+    	positionsTo.add(new Position("c2"));
+    	positionsTo.add(new Position("d2"));
+    	positionsTo.add(new Position("e1"));
+
+    	actualPossible.put(new Position("d1"), positionsTo);
+    	
+    	Map<Position, Set<Position>> calcedPossible = state.listPossibleMoves();   
+    	
+    	assertEquals(actualPossible, calcedPossible);
+    	
+	}
+	
+	@Test
+	public void testKingIntoCheck() {
+		
+		Map<Position, Set<Position>> actualPossible =  new HashMap<Position, Set<Position>>();
+    	Set<Position> positionsTo = new HashSet<Position>();
+		
+		state.placePiece(new King(Player.White), new Position("e1"));
+    	state.placePiece(new Bishop(Player.Black), new Position("g4"));
+    	
+    	positionsTo.add(new Position("d2"));
+    	positionsTo.add(new Position("f1"));
+    	positionsTo.add(new Position("f2"));
+
+    	actualPossible.put(new Position("e1"), positionsTo);
+    	
+    	Map<Position, Set<Position>> calcedPossible = state.listPossibleMoves();   
+    	
+    	assertEquals(actualPossible, calcedPossible);
+    	
+	}
+
+}

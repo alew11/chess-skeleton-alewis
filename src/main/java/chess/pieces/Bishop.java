d8a708b8babeabbdc0921313e5f3c9c40aeb1eab
alew11 <ajlewis1113@gmail.com>
2014-10-16 11:28:52 -0500

    Implement logic to move bishop

--- src/main/java/chess/pieces/Bishop.java
+++ src/main/java/chess/pieces/Bishop.java
@@ -1,5 +1,6 @@
 package chess.pieces;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -22,7 +23,50 @@ public class Bishop extends Piece {
 	@Override
 	public Set<Position> getPossibleMoves(
 			Map<Position, Piece> positionToPieceMap, Position originalPos) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Set<Position> possiblePositions = new HashSet<Position>();
+		
+		// Left, Up
+		possiblePositions.addAll(getMovesByDirection(positionToPieceMap, originalPos, -1, 1));
+		// Right, Up
+		possiblePositions.addAll(getMovesByDirection(positionToPieceMap, originalPos, 1, 1));
+		// Right, Down
+		possiblePositions.addAll(getMovesByDirection(positionToPieceMap, originalPos, 1, -1));
+		// Left, Down
+		possiblePositions.addAll(getMovesByDirection(positionToPieceMap, originalPos, -1, -1));
+
+		return possiblePositions;
+	}
+	
+	private Set<Position> getMovesByDirection(
+			Map<Position, Piece> positionToPieceMap, Position originalPos, int horiz, int vert) {
+		
+		Set<Position> possiblePositions = new HashSet<Position>();
+		Player player = positionToPieceMap.get(originalPos).getOwner();
+		char col = originalPos.getColumn();
+		int row = originalPos.getRow();
+		
+		col += horiz;
+		row += vert;
+		
+		// While the piece is still on the board
+		while (col >= Position.MIN_COLUMN && col <= Position.MAX_COLUMN && row >= Position.MIN_ROW && row <= Position.MAX_ROW) {
+			Position testPos = new Position(col, row);
+			
+			// If no piece is on the next square add the square to possibilities
+			if (positionToPieceMap.get(testPos) == null) {
+				possiblePositions.add(testPos);
+			} else {
+				// If the piece is the opponents add the square, else do not
+				if (!positionToPieceMap.get(testPos).getOwner().equals(player)) {
+					possiblePositions.add(testPos);
+				} 
+				break;
+			}
+			col += horiz;
+			row += vert;
+		}
+		
+		return possiblePositions;
 	}
 }
